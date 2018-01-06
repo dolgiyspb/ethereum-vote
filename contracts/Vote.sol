ef30bb1dbb53fc3a82517406b3c245383eae953a
Alexandr Dolgopolov <dolgiyspb@gmail.com>
2018-01-06 13:21:13 +0300

    Create simple vote contract

---
+++ contracts/Vote.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.0;
+
+contract Vote {
+    struct Candidate {
+        string name;
+        uint votesCount;
+    }
+
+    struct Voter {
+        bool voted;
+    }
+
+    address owner;
+
+    mapping(address => Voter) voters;
+    Candidate[3] candidates;
+
+    function Vote(string first, string second) public {
+        owner = msg.sender;
+        candidates[0] = Candidate({name: first, votesCount: 0});
+        candidates[1] = Candidate({name: second, votesCount: 0});
+        candidates[2] = Candidate({name: '', votesCount: 0}); // Against all candidate
+    }
+
+    function VoteFor(uint index) public {
+        require(index < candidates.length - 1);
+        Voter voter = voters[msg.sender];
+        require(!voter.voted);
+        candidates[index].votesCount++;
+        voter.voted = true;
+    }
+
+    function GetResults() public constant returns(uint, uint, uint) {
+        return (candidates[0].votesCount, candidates[1].votesCount, candidates[2].votesCount);
+    }
+
+    function Candidates() public constant returns(string, string) {
+        return (candidates[0].name, candidates[1].name);
+    }
+
+}
